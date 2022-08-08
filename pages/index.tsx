5ec64a89cc1ac4130931ffcc127f6d66b9245185
Chandan <chandancvp08@gmail.com>
2022-08-08 15:32:59 +0530

    done adding google login

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,18 @@
+
 import type { NextPage } from 'next'
+import { useSession } from 'next-auth/react'
 import Head from 'next/head'
 import Image from 'next/image'
 import Navbar from '../components/Navbar'
 import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
+  const {data:session} = useSession()
   return (
     <div>
       <Navbar/>
       <h1>home page</h1>
+      <p>{JSON.stringify(session)}</p>
     </div>
     )
 }
